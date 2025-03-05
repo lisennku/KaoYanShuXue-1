690508adee3bc5e2052a12e080e9ec2f74ebd819
Lisen Zhang <245062522@qq.com>
2025-03-05 17:55:48 +0800

    Code Simulate init

---
+++ CodeSim/det_calc.c
@@ -0,0 +1,11 @@
+/*
+ * @Author: Lisen Zhang 245062522@qq.com
+ * @Date: 2025-03-05 17:51:51
+ * @LastEditors: Lisen Zhang 245062522@qq.com
+ * @LastEditTime: 2025-03-05 17:53:17
+ * @FilePath: \KaoYanShuXue-1\CodeSim\det_calc.c
+ * @Description: 
+ *  模拟行列式计算过程
+ *  使用拉普拉斯定理，按行展开，可以输入行号
+ *  默认最大的行列式阶数为20
+ */
